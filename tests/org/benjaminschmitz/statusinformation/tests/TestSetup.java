d25be1d1e1ce94cf225ecd37d502ba08f1bea1d3
ge28dih <benjamin.schmitz@tum.de>
2021-08-06 16:40:20 +0200

    Test Setup

---
+++ tests/org/benjaminschmitz/statusinformation/tests/TestSetup.java
@@ -0,0 +1,14 @@
+package org.benjaminschmitz.statusinformation.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class TestSetup {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
