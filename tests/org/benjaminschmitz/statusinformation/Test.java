f318db71b33241d0d210ac3333fd14162cedee5e
ge28dih <benjamin.schmitz@tum.de>
2021-08-07 14:21:24 +0200

    Added Test dummy

---
+++ tests/org/benjaminschmitz/statusinformation/Test.java
@@ -0,0 +1,12 @@
+package org.benjaminschmitz.statusinformation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
